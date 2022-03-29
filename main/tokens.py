c3d9a9d5a6c5668b2c5181328ca6e9c9450183ac
Cole Parsons <coleparsons@Coles-MacBook-Pro.local>
2022-03-29 14:05:56 -0400

    2

--- main/tokens.py
+++ main/tokens.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from six import text_type
+#from six import text_type
 from django.contrib.auth.models import User
 #from .models import Profile
 
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            text_type(user.pk) + text_type(timestamp)
+            user.pk + timestamp
         )
 
 account_activation_token = AccountActivationTokenGenerator()
