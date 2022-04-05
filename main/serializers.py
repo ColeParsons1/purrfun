c89d31da206424b81fda4ae7474626134616c89d
Cole Parsons <coleparsons@Coles-MacBook-Pro.local>
2022-04-05 15:49:15 -0400

    25

--- main/serializers.py
+++ main/serializers.py
@@ -76,7 +76,6 @@ class PostSerializer(serializers.ModelSerializer):
 	Image3 = serializers.SerializerMethodField()
 	Image4 = serializers.SerializerMethodField()
 	RepostAuthor = serializers.SerializerMethodField()
-	Author_Profile = serializers.SerializerMethodField()
 	def get_Author(self, Post):
 		if Post.Author:
 			return Post.Author.username
@@ -108,11 +107,7 @@ class PostSerializer(serializers.ModelSerializer):
 	def get_RepostAuthor(self, Post):
 		if Post.RepostAuthor:
 			return Post.RepostAuthor.profile.Display_Name
-		return ""
-	def get_Author_Profile(self, Post):
-		if Post.Author_Profile:
-			return Post.Author_Profile.id
-		return "" 	                                 
+		return ""	                                 
    
 
 	class Meta:
